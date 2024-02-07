9022dddbc7255fd599a75fc877cae3a491ab9f15
matteo1610 <cardellini.matteo@gmail.com>
2024-02-07 16:07:27 +0100

    Added User class

---
+++ src/main/java/it/unicam/cs/exploremunicipalities/model/user/User.java
@@ -0,0 +1,35 @@
+package it.unicam.cs.exploremunicipalities.model.user;
+
+import java.util.UUID;
+
+public class User {
+    private final UUID id;
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.id = UUID.randomUUID();
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
