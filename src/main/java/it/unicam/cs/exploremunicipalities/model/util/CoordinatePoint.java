a73ccdb49fe937235575c085cadcea117e0db942
leonardo <leonardo.pigliacampo02@gmail.com>
2024-02-10 15:36:07 +0100

    Added record representing a geolocated point

---
+++ src/main/java/it/unicam/cs/exploremunicipalities/model/util/CoordinatePoint.java
@@ -0,0 +1,19 @@
+package it.unicam.cs.exploremunicipalities.model.util;
+
+/**
+ * This class represents a geographical point with latitude and longitude coordinates.
+ * The latitude and longitude values are in degrees.
+ *
+ * @param latitude The latitude of the point in degrees.
+ * @param longitude The longitude of the point in degrees.
+ * @throws IllegalArgumentException if the latitude is less than -90 or greater than 90, or if the longitude is less
+ *                                  than -180 or greater than 180.
+ */
+public record CoordinatePoint(double latitude, double longitude) {
+    public CoordinatePoint {
+        if (latitude < -90 || latitude > 90)
+            throw new IllegalArgumentException("Invalid latitude value: " + latitude);
+        if (longitude < -180 || longitude > 180)
+            throw new IllegalArgumentException("Invalid longitude value: " + longitude);
+    }
+}
