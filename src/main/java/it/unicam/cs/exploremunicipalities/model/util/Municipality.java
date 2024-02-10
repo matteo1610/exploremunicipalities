a686fbe86f03a080de66c3e6e6d80fd5066d07b5
leonardo <leonardo.pigliacampo02@gmail.com>
2024-02-10 15:38:28 +0100

    Added a class representing a municipality

---
+++ src/main/java/it/unicam/cs/exploremunicipalities/model/util/Municipality.java
@@ -0,0 +1,38 @@
+package it.unicam.cs.exploremunicipalities.model.util;
+
+import java.util.UUID;
+
+
+
+/**
+ * This class represents a municipality with a name, a province and an identity point.
+ */
+public final class Municipality {
+    private final UUID id;
+    private final String name;
+    private final String province;
+    private final CoordinatePoint identityPoint;
+
+    public Municipality(String name, String province, CoordinatePoint identityPoint) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.province = province;
+        this.identityPoint = identityPoint;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getProvince() {
+        return this.province;
+    }
+
+    public CoordinatePoint getIdentityPoint() {
+        return this.identityPoint;
+    }
+}
