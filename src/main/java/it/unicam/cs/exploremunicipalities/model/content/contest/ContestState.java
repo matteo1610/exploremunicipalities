fa1ddba48b2e5819b3281e52346ecce4c771ff86
leonardo <leonardo.pigliacampo02@gmail.com>
2024-02-15 11:41:34 +0100

    Added Contest class and ContestState

---
+++ src/main/java/it/unicam/cs/exploremunicipalities/model/content/contest/ContestState.java
@@ -0,0 +1,24 @@
+package it.unicam.cs.exploremunicipalities.model.content.contest;
+
+/**
+ * This enum represents the state of a contest.
+ */
+public enum ContestState {
+    CREATED,
+    OPEN,
+    CLOSED;
+
+    /**
+     * Returns the string representation of the contest state.
+     *
+     * @return the string representation of the contest state
+     */
+    public static ContestState fromString(String state) {
+        return switch (state) {
+            case "CREATED" -> CREATED;
+            case "OPEN" -> OPEN;
+            case "CLOSED" -> CLOSED;
+            default -> throw new IllegalArgumentException("Invalid contest state");
+        };
+    }
+}
