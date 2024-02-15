fea63a25a11663a45b74cabb00977e09d619652a
leonardo <leonardo.pigliacampo02@gmail.com>
2024-02-15 12:11:26 +0100

    Added enum for contribution types

---
+++ src/main/java/it/unicam/cs/exploremunicipalities/model/content/ContributionType.java
@@ -0,0 +1,24 @@
+package it.unicam.cs.exploremunicipalities.model.content;
+
+
+/**
+ * This enum represents the type of contribution that can be made by a user.
+ */
+public enum ContributionType {
+    POINT_OF_INTEREST,
+    EVENT,
+    ITINERARY;
+
+    /**
+     * Returns the string representation of the contribution type.
+     * @return the string representation of the contribution type
+     */
+    public static ContributionType fromString(String type) {
+        return switch (type) {
+            case "POINT_OF_INTEREST" -> POINT_OF_INTEREST;
+            case "EVENT" -> EVENT;
+            case "ITINERARY" -> ITINERARY;
+            default -> throw new IllegalArgumentException("Invalid contribution type");
+        };
+    }
+}
