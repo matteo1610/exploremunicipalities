bb6474b981075462c03fa47fdfdd54aea8d62d32
matteo1610 <cardellini.matteo@gmail.com>
2024-02-15 14:26:01 +0100

    Spring boot initialization

---
+++ src/main/java/it/unicam/cs/exploremunicipalities/ExploreMunicipalitiesApplication.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.exploremunicipalities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreMunicipalitiesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreMunicipalitiesApplication.class, args);
+    }
+}
