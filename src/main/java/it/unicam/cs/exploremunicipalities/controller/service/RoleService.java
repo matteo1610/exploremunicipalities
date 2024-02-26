25d1352ca9356989fa0ade2ff28eb660dbf126f3
leonardo <leonardo.pigliacampo02@gmail.com>
2024-02-26 15:22:17 +0100

    Defined interface for RoleService

--- src/main/java/it/unicam/cs/exploremunicipalities/controller/service/RoleService.java
+++ src/main/java/it/unicam/cs/exploremunicipalities/controller/service/RoleService.java
@@ -19,25 +19,12 @@ public class RoleService implements RoleServiceInterface {
         this.municipalityService = municipalityService;
     }
 
-    /**
-     * Get the roles associated in a municipality
-     * @param municipalityId id of the municipality
-     * @return the roles associated in a municipality
-     */
+    @Override
     public Set<UserRole> getRoles(long municipalityId) {
         return this.licenseRepository.findRolesByMunicipality(municipalityId);
     }
 
-    /**
-     * Set the license for a user in a municipality
-     * @param user user to set the license
-     * @param municipalityId id of the municipality
-     * @param role role of the user
-     * @throws IllegalArgumentException if the municipality not exists
-     * @throws IllegalArgumentException if the user not exists
-     * @throws IllegalArgumentException if the user role not exists
-     * @throws IllegalArgumentException if the municipality already has a curator or an animator
-     */
+    @Override
     public void setLicense(User user, long municipalityId, UserRole role) {
         switch (role) {
             case CONTRIBUTOR, AUTHORIZED_CONTRIBUTOR -> this.createLicense(user, municipalityId, role);
--- src/main/java/it/unicam/cs/exploremunicipalities/controller/service/abstractions/RoleServiceInterface.java
+++ src/main/java/it/unicam/cs/exploremunicipalities/controller/service/abstractions/RoleServiceInterface.java
@@ -1,4 +1,32 @@
 package it.unicam.cs.exploremunicipalities.controller.service.abstractions;
 
+import it.unicam.cs.exploremunicipalities.model.user.User;
+import it.unicam.cs.exploremunicipalities.model.user.UserRole;
+
+import java.util.Set;
+
 public interface RoleServiceInterface {
+
+    /**
+     * Get the roles associated in a municipality
+     * @param municipalityId id of the municipality
+     * @return the roles associated in a municipality
+     */
+    Set<UserRole> getRoles(long municipalityId);
+
+    /**
+     * Set the license for a user in a municipality
+     * @param user user to set the license
+     * @param municipalityId id of the municipality
+     * @param role role of the user
+     * @throws IllegalArgumentException if the municipality not exists
+     * @throws IllegalArgumentException if the user not exists
+     * @throws IllegalArgumentException if the user role not exists
+     * @throws IllegalArgumentException if the municipality already has a curator or an animator
+     */
+    void setLicense(User user, long municipalityId, UserRole role);
+
+
+
+
 }
