af3494a9164ce9cf09bd92e3b6f3912018a99824
leonardo <leonardo.pigliacampo02@gmail.com>
2024-02-28 10:41:50 +0100

    Defined interface NotificationService

--- src/main/java/it/unicam/cs/exploremunicipalities/service/NotificationService.java
+++ src/main/java/it/unicam/cs/exploremunicipalities/service/NotificationService.java
@@ -3,6 +3,7 @@ package it.unicam.cs.exploremunicipalities.service;
 import it.unicam.cs.exploremunicipalities.dto.NotificationDTO;
 import it.unicam.cs.exploremunicipalities.model.user.Notification;
 import it.unicam.cs.exploremunicipalities.model.user.User;
+import it.unicam.cs.exploremunicipalities.service.abstractions.NotificationServiceInterface;
 import it.unicam.cs.exploremunicipalities.service.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +11,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-public class NotificationService {
+public class NotificationService implements NotificationServiceInterface {
     private final NotificationRepository notificationRepository;
 
     public NotificationService(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
     }
 
-    /**
-     * Returns all the notifications of the user.
-     * @param user the user to get the notifications from
-     * @return all the notifications of the user
-     */
+    @Override
     public Set<NotificationDTO> getNotifications(User user) {
         Set<NotificationDTO> notifications = new HashSet<>();
         for (Notification n : user.getNotifications()) {
@@ -30,11 +27,7 @@ public class NotificationService {
         return notifications;
     }
 
-    /**
-     * Creates a new notification with the given message and adds it to the user.
-     * @param user the user to add the notification to
-     * @param notification the notification to be added to the user
-     */
+    @Override
     public void createNotification(User user, Notification notification) {
         user.addNotification(notification);
         this.notificationRepository.save(notification);
---
+++ src/main/java/it/unicam/cs/exploremunicipalities/service/abstractions/NotificationServiceInterface.java
@@ -0,0 +1,24 @@
+package it.unicam.cs.exploremunicipalities.service.abstractions;
+
+import it.unicam.cs.exploremunicipalities.dto.NotificationDTO;
+import it.unicam.cs.exploremunicipalities.model.user.Notification;
+import it.unicam.cs.exploremunicipalities.model.user.User;
+
+import java.util.Set;
+
+public interface NotificationServiceInterface {
+
+    /**
+     * Returns all the notifications of the user.
+     * @param user the user to get the notifications from
+     * @return all the notifications of the user
+     */
+    Set<NotificationDTO> getNotifications(User user);
+
+    /**
+     * Creates a new notification with the given message and adds it to the user.
+     * @param user the user to add the notification to
+     * @param notification the notification to be added to the user
+     */
+    void createNotification(User user, Notification notification);
+}
