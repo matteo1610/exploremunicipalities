14a8be0b13712edce25b74428a9c1e85914fe685
leonardo <leonardo.pigliacampo02@gmail.com>
2024-09-27 17:12:11 +0200

    Refactor login and sign-up components, and create component for contacts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { MunicipalityListComponent } from './Component/municipality-list/municipality-list.component';
 import { MunicipalityDetailComponent } from './Component/municipality-detail/municipality-detail.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './Component/home/home.component';
+import { ContactsComponent } from './Component/contacts/contacts.component';
+import { LoginComponent } from './Component/auth/login/login.component';
+import { SignUpComponent } from './Component/auth/sign-up/sign-up.component';
 
 
 export const routes: Routes = [
     {path : 'list', component: MunicipalityListComponent},   // Rotta per la lista di municipi
     {path : 'list/:id', component: MunicipalityDetailComponent},    // Rotta per il dettaglio di un singolo municipio
+    {path : 'contatti', component: ContactsComponent},    // Rotta per la pagina dei contatti
+    {path : 'home', component: HomeComponent},    // Rotta per la homepage
+    {path: 'login', component: LoginComponent},    // Rotta per la pagina di login
+    {path: 'signup', component: SignUpComponent},    // Rotta per la pagina di registrazione
     {path : '', component: HomeComponent},    // Rotta per la homepage
     { path: '**', redirectTo: '', pathMatch: 'full' }  // Gestione delle rotte non esistenti (404 redirect alla homepage)
 ];
