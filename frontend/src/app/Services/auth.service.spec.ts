43a0c4693f7dc5f4f10dd7c9526a0c5c0df225f6
leonardo <leonardo.pigliacampo02@gmail.com>
2024-10-07 18:25:56 +0200

    Refactor service test files to conform to directory structure

--- frontend/src/app/Services/auth.service.spec.ts
+++ frontend/src/app/Services/auth.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './Services/auth.service';
+import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
